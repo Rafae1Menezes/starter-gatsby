6e8d962dc5c9399dfb77a3ab78b52ac2fbc26948
Rafael Menezes <menezes.inbox@gmail.com>
2022-12-02 15:37:53 -0300

    feat: wip2

--- src/components/myComponent/index.tsx
+++ src/components/myComponent/index.tsx
@@ -1,15 +1,19 @@
 import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
+import CaixaDeSom from "../../images/caixa-de-som.svg";
+import * as styles from "./index.module.scss";
 
 export const MyComponent = () => (
   <>
-    <h1>Hello World</h1>
+    <h2 className={styles.titulo}>Meu Componente</h2>
     <StaticImage
       src="../../images/artistas.png"
       alt="Artistas"
       placeholder="blurred"
-      width={240}
-      height={180}
+      width={374}
+      height={346}
+      className={styles.image1}
     />
+    <CaixaDeSom className={styles.image2} />
   </>
 );
