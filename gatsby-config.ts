1f7175b542551bd21986ab729c33283d32bb338b
Rafael Menezes <menezes.inbox@gmail.com>
2022-12-04 12:05:33 -0300

    chore: configura typescript

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,21 +2,29 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `gatsby-starter`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: "gatsby-starter",
+    siteUrl: "https://www.yourdomain.tld"
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }]
+  graphqlTypegen: {
+    typesOutputPath: "src/types/gatsby-types.d.ts"
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/"
+      },
+      __key: "images"
+    }
+  ]
 };
 
 export default config;
