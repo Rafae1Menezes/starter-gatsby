f0786afdce07d262d2dcefe7852e94e05cc9b558
Rafael Menezes <menezes.inbox@gmail.com>
2022-12-04 12:59:47 -0300

    chore: configura svg

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,9 +5,6 @@ const config: GatsbyConfig = {
     title: "gatsby-starter",
     siteUrl: "https://www.yourdomain.tld"
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: {
     typesOutputPath: "src/types/gatsby-types.d.ts"
   },
@@ -23,6 +20,14 @@ const config: GatsbyConfig = {
         path: "./src/images/"
       },
       __key: "images"
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /images/
+        }
+      }
     }
   ]
 };
